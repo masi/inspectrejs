4bb7d51dff5843bb64c3c9f8eeeb7408b576c910
Martin Kutschker <mk@plan2.net>
2015-04-17 21:04:21 +0200

    sanitize command name (user input)

--- inspectre.js
+++ inspectre.js
@@ -11,7 +11,7 @@ if (system.args.length < 2) {
 	console.error("Usage: <phantomjs|slimerjs> inspectre.js <command>");
 	phantom.exit(1);
 } else {
-	command = system.args[1]; // @todo sanitise input
+	command = system.args[1].replace(/[^a-z]/g, '');
 	try {
 		Command = require('./lib/cmd/' + command + '.js');
 	} catch (error) {
